a9470adc0d3dfcc023415265757b6cdb0ea471cd
Gilvan Ribeiro De Almeida <g.r.almeida@live.com>
2022-09-03 00:05:41 -0300

    Textos generalizados

--- App.js
+++ App.js
@@ -2,7 +2,8 @@ import React from 'react';
 import { StatusBar, SafeAreaView, View } from 'react-native';
 import { useFonts, Montserrat_400Regular, Montserrat_700Bold } from '@expo-google-fonts/montserrat';
 
-import Cesta from './src/telas/Cesta'
+import Cesta from './src/telas/Cesta';
+import mock from './src/mocks/cesta';
 
 export default function App() {
   const [fonteCarregada] = useFonts({
@@ -17,7 +18,7 @@ export default function App() {
   return (
     <SafeAreaView>
       <StatusBar />
-      <Cesta />
+      <Cesta {...mock} />
     </SafeAreaView>
   );
 }
