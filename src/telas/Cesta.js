5435d821b3457f52a40af64b6d7438b32f0774e5
Gilvan Ribeiro De Almeida <g.r.almeida@live.com>
2022-09-02 23:31:09 -0300

    Componente de texto

--- src/telas/Cesta.js
+++ src/telas/Cesta.js
@@ -1,6 +1,8 @@
 import React from 'react';
 import { View, Dimensions, Image, StyleSheet, Text } from 'react-native';
 
+import Texto from '../componentes/Texto';
+
 import topo from '../../assets/topo.png';
 import logo from '../../assets/logo.png';
 
@@ -9,19 +11,19 @@ const width = Dimensions.get('screen').width;
 export default function Cesta() {
     return <>
         <Image source={topo} style={estilos.topo} />
-        <Text style={estilos.titulo}>Detalhe da cesta</Text>
+        <Texto style={estilos.titulo}>Detalhe da cesta</Texto>
 
         <View style={estilos.cesta}>
-            <Text style={estilos.nome}>Cesta de Verduras</Text>
+            <Texto style={estilos.nome}>Cesta de Verduras</Texto>
             <View style={estilos.fazenda}>
                 <Image style={estilos.imagemFazenda} source={logo} />
                 <Text style={estilos.nomeFazenda}>Jenny Jack Farm</Text>
             </View>
-            <Text style={estilos.descricao}>
+            <Texto style={estilos.descricao}>
                 Uma cesta com produtos selecionados
                 cuidadosamente da fazenda direto
                 para sua cozinha
-            </Text>
+            </Texto>
             <Text style={estilos.preco}>R$ 40,00</Text>
         </View>
     </>
@@ -50,7 +52,7 @@ const estilos = StyleSheet.create({
         color: '#464646',
         fontSize: 26,
         lineHeight: 42,
-        fontFamily: 'MontserratBold'
+        fontWeight: 'bold'
     },
     fazenda: {
         flexDirection: 'row',
@@ -63,8 +65,7 @@ const estilos = StyleSheet.create({
     nomeFazenda: {
         fontSize: 16,
         lineHeight: 26,
-        marginLeft: 12,
-        fontFamily: 'MontserratRegular'
+        marginLeft: 12
     },
     descricao: {
         color: '#A3A3A3',
